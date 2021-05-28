6c4c4e3dfb469eafd56ffc22d871332815f2ae44
jpire <jpire.dev@gmail.com>
2021-05-28 15:35:53 +0200

    fix(template): cms data implementation

--- src/components/home/Action.js
+++ src/components/home/Action.js
@@ -8,9 +8,9 @@ const Action = () => {
   const { action } = useContentAction();
   return (
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">{file.childMarkdownRemark.frontmatter.title}</h3>
+      <h3 className="text-5xl font-semibold">{action.childMarkdownRemark.frontmatter.title}</h3>
       <div className="mt-8 text-xl font-light">
-        <p dangerouslySetInnerHTML={{ __html: file.childMarkdownRemark.html }}></p>
+        <p dangerouslySetInnerHTML={{ __html: action.childMarkdownRemark.html }}></p>
       </div>
       <p className="mt-8">
         <Button size="xl">Découvrir</Button>
--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -4,25 +4,32 @@ import HeroImage from '../../svg/HeroImage';
 import Button from '../../components/Button';
 
 import { useContentHero } from '../../hooks/use-content-hero';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Hero = () => {
   const { hero } = useContentHero();
+  const heroImagePublicPath = '../../../static' + hero.childMarkdownRemark.frontmatter.image;
   return (
     <div className="container mx-auto px-8 lg:flex">
-      {/* <div className="text-center lg:text-left lg:w-1/2">
+      <div className="text-center lg:text-left lg:w-1/2">
         <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-          {file.childMarkdownRemark.frontmatter.title}
+          {hero.childMarkdownRemark.frontmatter.title}
         </h1>
         <p className="text-xl lg:text-2xl mt-6 font-light">
-          {file.childMarkdownRemark.frontmatter.action}
+          {hero.childMarkdownRemark.frontmatter.action}
         </p>
         <p className="mt-8 md:mt-12">
           <Button size="lg">Découvrir</Button>
         </p>
       </div>
       <div className="lg:w-1/2">
-        <HeroImage />
-      </div> */}
+        {heroImagePublicPath}
+        <StaticImage
+          src={'../../../static/assets/heroImage.svg'}
+          formats={['auto', 'webp', 'avif']}
+          alt={hero.childMarkdownRemark.frontmatter.title}
+        />
+      </div>
     </div>
   );
 };
